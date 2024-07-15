ab4a6ee64e530210cf51ead5c103eae06e59fc72
Gablrx <97037067+Gablrx@users.noreply.github.com>
2024-07-15 12:02:06 +0200

    add coms

--- inc/get-random-photo.php
+++ inc/get-random-photo.php
@@ -1,24 +1,27 @@
 <?php
+
+// Récupére l'URL d'une photo aléatoire au format paysage Pour la bannière
 function get_random_photo()
 {
-    $args = array(
-        'post_type' => 'photo',
-        'posts_per_page' => 1,
-        'orderby' => 'rand',
-        'tax_query' => array(
+    $randomPhoto = array(
+        'post_type' => 'photo', // CPT 'photo'
+        'tax_query' => array( // Uniquement le format 'paysage'
             array(
                 'taxonomy' => 'photo_formats',
-                'field'    => 'slug',
+                'field'    => 'slug', // 
                 'terms'    => 'paysage',
             ),
         ),
+        'orderby' => 'rand', // Tri aléatoire
+        'posts_per_page' => 1, // Une seule photo
     );
-    $query = new WP_Query($args);
-    if ($query->have_posts()) {
+
+    $query = new WP_Query($randomPhoto);
+    if ($query->have_posts()) { // Vérifie s'il y a des posts correspondants à la requête
         $query->the_post();
-        $photo_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-        wp_reset_postdata();
-        return $photo_url;
+        $photo_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); // Récupère l'URL de la photo taille full
+        wp_reset_postdata(); // Réinitialise les données
+        return $photo_url; // retourne l'url de la photo
     }
-    return false;
+    return false; // Si pas de photo ttrouvée retourne false
 }
